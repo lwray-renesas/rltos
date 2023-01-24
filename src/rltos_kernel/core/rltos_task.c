b2eafe0528e6d1906e3cef51f5798654c5ad2b10
wraydev <louis_wraydev@outlook.com>
2023-01-24 16:54:13 +0000

    removed unnecessary cast

--- src/rltos_kernel/core/rltos_task.c
+++ src/rltos_kernel/core/rltos_task.c
@@ -31,28 +31,28 @@ void Rltos_task_create(p_rltos_task_t const task_to_create, stack_ptr_type const
 	stack_ptr_type l_p_stack_top = Rltos_port_stack_init(p_stack_top, p_task_func);
 
 	/* Initialise the task*/
-	Task_init((p_rltos_task_t)(task_to_create), l_p_stack_top, p_task_func, run_task);
+	Task_init(task_to_create, l_p_stack_top, p_task_func, run_task);
 }
 /* END OF FUNCTION*/
 
 void Rltos_task_destroy(p_rltos_task_t const task_to_destroy)
 {
 	/* Denitialise the task*/
-	Task_deinit((p_rltos_task_t)(task_to_destroy));
+	Task_deinit(task_to_destroy);
 }
 /* END OF FUNCTION*/
 
 void Rltos_task_stop(p_rltos_task_t const task_to_stop)
 {
-	Task_set_stopped((p_rltos_task_t)(task_to_stop));
+	Task_set_stopped(task_to_stop);
 
-	Task_yield_if_current_task((p_rltos_task_t)(task_to_stop));
+	Task_yield_if_current_task(task_to_stop);
 }
 /* END OF FUNCTION*/
 
 void Rltos_task_resume(p_rltos_task_t const task_to_resume)
 {
-	Task_resume((p_rltos_task_t)(task_to_resume));
+	Task_resume(task_to_resume);
 }
 /* END OF FUNCTION*/
 
